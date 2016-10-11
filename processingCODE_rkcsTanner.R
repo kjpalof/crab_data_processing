59e3edb3cb4eda418c226a94f51a394b601addda
Katie Palof <katie.palof@alaska.gov>
2016-10-11 10:04:21 -0800

    RKCS data processing code.  Different due to lack of strata weighting.

--- processingCODE_rkcsTanner.R
+++ processingCODE_rkcsTanner.R
@@ -24,7 +24,7 @@ library(weights)
 dat <- read.csv("./data/rkc_tanner/RKCS_forTanner.csv")
                   # this is input from OceanAK - set up as red crab survey data for CSA
 #area <- read.csv("./data/Juneau/Juneau_Barlow_strata_area.csv") 
-                  #this file is the same every year.  Unless the survey methods change
+                #NO area for this data since these are stratified by RKCS area.  
 #histdat <- read.csv("./data/Juneau/2Juneau Stratified CPUE 2015_area formula.csv")
                   ## !!!!  In future years this file will be 'JNU_CPUE_ALL' and just get updated with current years data.
 #females <- read.csv("./data/Juneau/RKC_11_16_large females_by_pot.csv")
@@ -68,9 +68,9 @@ dat1ab %>%
                               ifelse(Sex.Code ==1 & Width.Millimeters>109 & Width.Millimeters < 138,'Pre_Recruit', 
                                ifelse(Sex.Code ==1 & Width.Millimeters > 137 & Width.Millimeters <170 & Shell.Condition.Code <4, 'Recruit',
                                 ifelse(Sex.Code ==1 & Width.Millimeters >169|(Shell.Condition.Code >3 & Width.Millimeters >137), 'Post_Recruit', 
-                                  ifelse(Sex.Code ==2 & Egg.Development.Code==4, 'Small.Female', 
-                                         ifelse(Sex.Code ==2 & Width.Millimeters>0, 'Large.Female', 'Missing')))))))) -> Tdat1
-write.csv(Tdat1, './results/problemstanner.csv')
+                                  ifelse(Sex.Code ==2 & Egg.Development.Code==4, 'Small.Females', 
+                                         ifelse(Sex.Code ==2 & Width.Millimeters>0, 'Large.Females', 'Missing')))))))) -> Tdat1
+#write.csv(Tdat1, './results/problemstanner2.csv')
 # need to STOP here and fix problems with data.  Not sure why some females are showing up as NA....also need to fill in missing 
 #  widths and density strata (although this applies to red crab NOT tanner)
 ##################################################################
@@ -79,23 +79,14 @@ write.csv(Tdat1, './results/problemstanner.csv')
 #Now summarize by pot - remember to keep areas seperate.
 #Need Number of Specimens by recruit class
 Tdat1 %>%
-  group_by(Year, AREA, Pot.No, Density.Strata.Code, mod_recruit) %>% # use AREA here instead of location due to multiple location names for one survey area
+  group_by(Year, AREA, Pot.No, mod_recruit) %>% # use AREA here instead of location due to multiple location names for one survey area
   summarise(crab = sum(Number.Of.Specimens)) -> dat2
 
-dat3 <- dcast(dat2, Year + AREA + Pot.No +Density.Strata.Code ~ mod_recruit, sum, drop=TRUE)
+dat3 <- dcast(dat2, Year + AREA + Pot.No ~ mod_recruit, sum, drop=TRUE)
 
-dat3 %>%
-  select(NA > 0)
 #head(dat3)# check to make sure things worked.
 
-# Join area input file with dat3 - which is the data summarized by pot.  Each sampling area has it's own area file or area per
-#     strata.  This is used to calculating the weighting for weighted CPUE.
-dat3 %>%
-  right_join(area) -> tab
-#Calculates the number of pots per strata.  
-tab %>%
-  group_by(Year, Location, Density.Strata.Code) %>%
-  summarise(npots  = length(Pot.No)) -> pots_per_strata
+# No weighting by strata here for RKCS data due to it being designed for RKC.
 
 ##################################################################
 ##### Weighted CPUE current year -----------------------------------
@@ -103,28 +94,23 @@ tab %>%
 #the weighting is the product of the area for each strata and the inverse (1/n) of the number of pots per strata per year
 # need to combine data sets to accomplish this.
 
-tab %>%
-  right_join(pots_per_strata) -> dat4
-
-dat4 %>%
-  mutate(inverse_n = 1 / npots, weighting = inverse_n * Area) ->dat5
-dat5 %>%
-  rename(Missing = Var.5, Large.Females = `Large Females`, Small.Females = `Small Females`) -> dat5
+#dat3 %>%
+ # rename(Missing = Var.5, Large.Females = `Large Females`, Small.Females = `Small Females`) -> dat5
 # this is neccessary so that current years file (dat5) matches the historic file names
 
 #This version is ready to calculate CPUE for each recruit class
 #Calculates a weighted mean CPUE and SE for each recruit class
-dat5 %>%
-  group_by(Year) %>%
-  summarise(Pre_Recruit_wt = wt.mean(Pre_Recruit, weighting), PreR_SE = (wt.sd(Pre_Recruit, weighting)/(sqrt(sum(!is.na(Pre_Recruit))))), 
-            Recruit_wt = wt.mean(Recruit, weighting), Rec_SE = (wt.sd(Recruit, weighting)/(sqrt(sum(!is.na(Recruit))))), 
-            Post_Recruit_wt = wt.mean(Post_Recruit, weighting), PR_SE = (wt.sd(Post_Recruit, weighting)/(sqrt(sum(!is.na(Post_Recruit))))),
-            Juvenile_wt = wt.mean(Juvenile, weighting), Juv_SE = (wt.sd(Juvenile, weighting)/(sqrt(sum(!is.na(Juvenile))))), 
-            MatF_wt = wt.mean(Large.Females, weighting), MatF_SE = (wt.sd(Large.Females, weighting)/(sqrt(sum(!is.na(Large.Females))))),
-            SmallF_wt = wt.mean(Small.Females, weighting), SmallF_SE = (wt.sd(Small.Females, weighting)/(sqrt(sum(!is.na(Small.Females)))))) -> CPUE_wt_16
+dat3 %>%
+  group_by(AREA, Year) %>%
+  summarise(Pre_Recruit_u = mean(Pre_Recruit), PreR_SE = (sd(Pre_Recruit)/(sqrt(sum(!is.na(Pre_Recruit))))), 
+            Recruit_u = mean(Recruit), Rec_SE = (sd(Recruit)/(sqrt(sum(!is.na(Recruit))))), 
+            Post_Recruit_u = mean(Post_Recruit), PR_SE = (sd(Post_Recruit)/(sqrt(sum(!is.na(Post_Recruit))))),
+            Juvenile_u = mean(Juvenile), Juv_SE = (sd(Juvenile)/(sqrt(sum(!is.na(Juvenile))))), 
+            MatF_u = mean(Large.Females), MatF_SE = (sd(Large.Females)/(sqrt(sum(!is.na(Large.Females))))),
+            SmallF_u = mean(Small.Females), SmallF_SE = (sd(Small.Females)/(sqrt(sum(!is.na(Small.Females)))))) -> CPUE_13_16
 # check to confirm last years CPUEs match - that's why we use two years.
 # change name and folder for each area
-write.csv(CPUE_wt_16, './results/Juneau/JNU_CPUE_16.csv')
+write.csv(CPUE_13_16, './results/RKCS_CPUE_13_16.csv')
 
 ##################################################################
 ##### Historic file ---------------------------------------
@@ -133,48 +119,48 @@ write.csv(CPUE_wt_16, './results/Juneau/JNU_CPUE_16.csv')
 # any issues with recalculating the crab per pot due to edits in data.
 # read in historic by pot file and make sure variable names match
 
-historicdata <- histdat[,1:15] # remove last two column of total crab and 
+#historicdata <- histdat[,1:15] # remove last two column of total crab and 
  # has all data from 2001 to 2015
 
 # need to add 2016 to historicdata file
 # Locations in historic file are numbers.  Here I have names, should I change this?
 # only 2016 data 
-dat5 %>%
-  filter(Year == 2016) -> dat5_2016 
-CPUE_ALL_YEARS <- rbind(historicdata, dat5_2016)
+#dat5 %>%
+#  filter(Year == 2016) -> dat5_2016 
+#CPUE_ALL_YEARS <- rbind(historicdata, dat5_2016)
 # this is the final file by pot.  Now this file can be summarized to give CPUE by year like above (see dat 5 to CPUE_wt_JNU_2016)
 # change same of folder and file.
-write.csv(CPUE_ALL_YEARS, './results/Juneau/JNU_perpot_all_16.csv')
+write.csv(dat3, './results/RKCS_perpot_allyears.csv')
 
 ##################################################################
 ##### Short term trends -------------------------------------
 ##################################################################
 #look at trend for the last 4 years.  Need a file with last four years in to JNU_CPUE_ALL
-CPUE_ALL_YEARS %>%
+dat3 %>%
   filter(Year >=2013) -> BYPOT_ST_16 # short term file has last 4 years in it
 
 plot(BYPOT_ST_16$Year, BYPOT_ST_16$Juvenile)
-Juv_fit <-lm(Juvenile ~ Year, data = BYPOT_ST_16, weights = weighting)
+Juv_fit <-lm(Juvenile ~ Year, data = BYPOT_ST_16)
 abline(Juv_fit, col= 'red')
 summary(Juv_fit)
 
 plot(BYPOT_ST_16$Year, BYPOT_ST_16$Large.Females)
-Lfem_fit <-lm(Large.Females ~ Year, data = BYPOT_ST_16, weights = weighting)
+Lfem_fit <-lm(Large.Females ~ Year, data = BYPOT_ST_16)
 abline(Lfem_fit, col= 'red')
 summary(Lfem_fit)
 
 plot(BYPOT_ST_16$Year, BYPOT_ST_16$Post_Recruit)
-PR_fit <-lm(Post_Recruit ~ Year, data = BYPOT_ST_16, weights = weighting)
+PR_fit <-lm(Post_Recruit ~ Year, data = BYPOT_ST_16)
 abline(PR_fit, col= 'red')
 summary(PR_fit)
 
 plot(BYPOT_ST_16$Year, BYPOT_ST_16$Pre_Recruit)
-PreR_fit <-lm(Pre_Recruit ~ Year, data = BYPOT_ST_16, weights = weighting)
+PreR_fit <-lm(Pre_Recruit ~ Year, data = BYPOT_ST_16)
 abline(PreR_fit, col= 'red')
 summary(PreR_fit)
 
 plot(BYPOT_ST_16$Year, BYPOT_ST_16$Recruit)
-R_fit <-lm(Recruit ~ Year, data = BYPOT_ST_16, weights = weighting)
+R_fit <-lm(Recruit ~ Year, data = BYPOT_ST_16)
 abline(R_fit, col= 'red')
 summary(R_fit)
 
