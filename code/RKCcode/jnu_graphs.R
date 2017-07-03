28e54319f8b37963c3f7386d96a0455dd20de4fe
Katie Palof <katie.palof@alaska.gov>
2017-07-03 11:04:46 -0800

    initial code for creating graphs in R instead of Sigma plot

---
+++ code/RKCcode/jnu_graphs.R
@@ -0,0 +1,9 @@
+# K.Palof 7-3-17
+# attempt to create graphs in R instead of Sigma Plot
+
+### Load packages -----
+
+
+
+#### Load data -----
+biomass <- read.csv("./data/redcrab/Juneau/biomass_historic.csv")
