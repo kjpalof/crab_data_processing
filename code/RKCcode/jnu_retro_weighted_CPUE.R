1b89eb0499db746f8cd520b2f9c6b3ead7168eb8
Katie Palof <katie.palof@alaska.gov>
2017-07-05 13:56:27 -0800

    check data for missing or mistakes

--- code/RKCcode/jnu_retro_weighted_CPUE.R
+++ code/RKCcode/jnu_retro_weighted_CPUE.R
@@ -29,4 +29,66 @@ jnu_lat_long %>%
   summarise(n = n()) -> jnu_lat_long_bypot
 write.csv(jnu_lat_long_bypot, './results/redcrab/Juneau/jnu_lat_long_bypot.csv', row.names = FALSE)
 
+##### Initial review of new data -------------------------------------
+# remove pots with Pot condition code that's not "normal" or 1 
+levels(dat$Pot.Condition)
+dat %>% 
+  filter(Pot.Condition == "Normal"|Pot.Condition == "Not observed") -> dat1
+
+dat1 %>% filter(Recruit.Status == "", Number.Of.Specimens >= 1)# this SHOULD produce NO rows. 
+dat1 %>%
+  filter(Recruit.Status == "", Length.Millimeters >= 1) # this SHOULD produce NO rows.  If it does you have data problems go back and correct
+# before moving forward.
+dat1 %>% filter(!(Recruit.Status == "" & Number.Of.Specimens >= 1))->dat1 # removed rows without lengths to determine recruit class
+# also need to check soak time and to make sure all crab that were measured have a recruit status
+#come back later and add a soak time column - RKC soak time should be between 18-24??? double check this
+
 #### crab data processing ------------
+##### By Pot -------------------------------
+#Now summarize by pot - remember to keep areas seperate.
+dat1 %>%
+  group_by(Year, Location, Pot.No, Density.Strata.Code) %>%
+  summarise (total_crab = sum(Number.Of.Specimens)) #gets you total crab per pot.
+
+# need Number of Specimens by recruit class
+dat1 %>%
+  group_by(Year, Location, Pot.No, Density.Strata.Code, Recruit.Status) %>%
+  summarise(crab = sum(Number.Of.Specimens)) -> dat2
+
+dat3 <- dcast(dat2, Year + Location + Pot.No +Density.Strata.Code ~ Recruit.Status, sum, drop=TRUE)
+
+head(dat3)
+
+# Join area input file with dat3 - which is the data summarized by pot.  Each sampling area has it's own area file or area per
+#     strata.  This is used to calculating the weighting for weighted CPUE.
+dat3 %>%
+  right_join(area) -> tab
+#Calculates the number of pots per strata.  
+tab %>%
+  group_by(Year, Location, Density.Strata.Code) %>%
+  summarise(npots  = length(Pot.No)) -> pots_per_strata
+
+#####Weighted CPUE current year -----------------------------------
+#the weighting is the product of the area for each strata and the inverse (1/n) of the number of pots per strata per year
+# need to combine data sets to accomplish this.
+
+tab %>%
+  right_join(pots_per_strata) -> dat4
+
+dat4 %>%
+  mutate(inverse_n = 1 / npots, weighting = inverse_n * Area) ->dat5
+dat5 %>%
+  rename(Missing = Var.5, Large.Females = `Large Females`, Small.Females = `Small Females`) -> dat5
+
+#This version is ready to calculate CPUE for each recruit class
+#Calculates a weighted mean CPUE and SE for each recruit class
+dat5 %>%
+  group_by(Year) %>%
+  summarise(Pre_Recruit_wt = wt.mean(Pre_Recruit, weighting), PreR_SE = (wt.sd(Pre_Recruit, weighting)/(sqrt(sum(!is.na(Pre_Recruit))))), 
+            Recruit_wt = wt.mean(Recruit, weighting), Rec_SE = (wt.sd(Recruit, weighting)/(sqrt(sum(!is.na(Recruit))))), 
+            Post_Recruit_wt = wt.mean(Post_Recruit, weighting), PR_SE = (wt.sd(Post_Recruit, weighting)/(sqrt(sum(!is.na(Post_Recruit))))),
+            Juvenile_wt = wt.mean(Juvenile, weighting), Juv_SE = (wt.sd(Juvenile, weighting)/(sqrt(sum(!is.na(Juvenile))))), 
+            MatF_wt = wt.mean(Large.Females, weighting), MatF_SE = (wt.sd(Large.Females, weighting)/(sqrt(sum(!is.na(Large.Females))))),
+            SmallF_wt = wt.mean(Small.Females, weighting), SmallF_SE = (wt.sd(Small.Females, weighting)/(sqrt(sum(!is.na(Small.Females)))))) -> CPUE_wt_JNU_17
+
+write.csv(CPUE_wt_JNU_17, './results/redcrab/Juneau/JNU_CPUE_17.csv', row.names = FALSE)
