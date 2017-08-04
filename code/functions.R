27381fa82fe5c366364e2e9c3bd3f9663ba34c82
Katie Palof <katie.palof@alaska.gov>
2017-08-04 13:59:57 -0800

    weights for male crab function

--- code/functions.R
+++ code/functions.R
@@ -39,5 +39,36 @@ short_t <- function(bypot_st, year, area) {
     mutate(significant = ifelse(p.value < 0.05 & slope > 0, 1,
                                 ifelse(p.value <0.05 & slope <0, -1, 0))) %>%
     mutate(score = 0.25*significant) -> short_term_results
-  write_csv(short_term_results, paste0('results/redcrab/', area, '/ei_shortterm.csv'))
+  write_csv(short_term_results, paste0('results/redcrab/', area, '/shortterm.csv'))
+}
+
+
+### Long term function -------------------
+# need current years data and file with long term means
+
+long_t <- function(dat5_current, year, area) {
+  
+}
+
+
+
+### weights from weight -length relationship ------------
+weights <- function(dat1, slope, intercept, area){
+  dat1 %>%
+    mutate(weight_lb = (exp((slope*log(Length.Millimeters))-intercept))*(2.2/1000)) -> dat1
+  
+  Mature = c("Pre_Recruit", "Recruit", "Post_Recruit")
+  Legal =c("Recruit", "Post_Recruit")
+  # summary of weights all together - would like these in one calc and one summary table
+  dat1 %>% 
+    group_by(Year) %>% 
+    filter(Sex.Code == 1) %>% 
+    summarise(mature_lbs = wt.mean(weight_lb[Recruit.Status %in% Mature], 
+                                   Number.Of.Specimens[Recruit.Status %in% Mature]), 
+              legal_lbs = wt.mean(weight_lb[Recruit.Status %in% Legal], 
+                                  Number.Of.Specimens[Recruit.Status %in% Legal]), 
+              prer_lbs = wt.mean(weight_lb[Recruit.Status == "Pre_Recruit"], 
+                                 Number.Of.Specimens[Recruit.Status == "Pre_Recruit"])) -> male_weights
+  # final results with score - save here
+  write_csv(male_weights, paste0('results/redcrab/', area, '/maleweights.csv'))
 }
