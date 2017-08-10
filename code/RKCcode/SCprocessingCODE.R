32a0b4cd27787ae7df0d3d0e7e44adcde6ad3254
Katie Palof <katie.palof@alaska.gov>
2017-08-10 15:01:11 -0800

    code to calculate raw sample size

--- code/RKCcode/SCprocessingCODE.R
+++ code/RKCcode/SCprocessingCODE.R
@@ -264,6 +264,14 @@ egg_percent(LgF_dat1, 'Seymour', 2017)
 total_health('Seymour', 2017)
 # works as long as all files are saved in folder with area name
 
+### raw sample size -----------
+head(dat5)
+dat5 %>% group_by(Year, Location) %>%  select(Year, Location, Juvenile, Small.Females, 
+                                              Large.Females, Pre_Recruit, Recruit,Post_Recruit) %>% 
+  summarise_all(funs(sum)) -> raw_samp
+write.csv(raw_samp, './results/redcrab/Seymour/raw_sample.csv')
+
+
 ##### Restrospective Analysis -----------------------------------
 head(CPUE_ALL_YEARS)
 
