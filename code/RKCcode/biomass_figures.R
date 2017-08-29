4cc019819bf3a5bd0f9faff8483e87c7a2d7dc81
Katie Palof <katie.palof@alaska.gov>
2017-08-29 13:21:49 -0800

    packages and data sections

--- code/RKCcode/biomass_figures.R
+++ code/RKCcode/biomass_figures.R
@@ -1,3 +1,18 @@
 # K.Palof    8-29-17
 # Figure creation in response to 8-29-17 RKC meeting.
 # wants figures for each area with legal and mature biomass.  
+
+# Load packages -------------
+library(tidyverse)
+library(extrafont)
+library(grid)
+library(gridExtra)
+#font_import()
+loadfonts(device="win")
+windowsFonts(Times=windowsFont("TT Times New Roman"))
+
+theme_set(theme_bw(base_size=12,base_family='Times New Roman')+ 
+            theme(panel.grid.major = element_blank(),
+                  panel.grid.minor = element_blank()))
+#Load data ----------------
+biomass <- read.csv("./data/redcrab/biomass.csv")
