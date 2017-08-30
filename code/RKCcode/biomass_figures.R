0299f794fe9c93f4d4a3dc682d09e857c99fa889
Katie Palof <katie.palof@alaska.gov>
2017-08-30 10:30:33 -0800

    new data - use output from 2017 model, do not have historic point estimates for mature biomass past 2003.  Create graphs for each area

--- code/RKCcode/biomass_figures.R
+++ code/RKCcode/biomass_figures.R
@@ -4,6 +4,7 @@
 
 # Load packages -------------
 library(tidyverse)
+library(readxl)
 library(extrafont)
 library(grid)
 library(gridExtra)
@@ -15,25 +16,90 @@ theme_set(theme_bw(base_size=12,base_family='Times New Roman')+
             theme(panel.grid.major = element_blank(),
                   panel.grid.minor = element_blank()))
 #Load data ----------------
-biomass <- read.csv("./data/redcrab/biomass.csv")
+#biomass <- read.csv("./data/redcrab/biomass.csv") no record of historic mature biomass point estimates
+# in each year so using 2017 model output
+
+biomass <- read_excel(path = "./data/redcrab/2017_biomass_model.xlsx")
 
 # Area figures ---------------------
+# pybus ---------------
+biomass %>% filter(Location == "Pybus") %>% 
+  select(Year, legal, mature) ->pb.biomass
+pb.biomass_long <- gather(pb.biomass, type, pounds, legal:mature, factor_key = TRUE)
+
+pb <- ggplot(pb.biomass_long, aes(Year, pounds, group = type))+ 
+  geom_point(aes(color = type, shape = type), size =3) +
+  geom_line(aes(color = type, group = type))+
+  scale_colour_manual(name = "", values = c("grey1", "grey1"))+
+  scale_shape_manual(name = "", values = c(16, 1))+
+  
+  ylim(0,325000) +ggtitle("Pybus Bay 2017 Model") + ylab("Biomass (lbs)")+ xlab("Year")+
+  theme(plot.title = element_text(hjust =0.5)) + 
+  scale_x_continuous(breaks = seq(min(1993),max(2017), by =2)) +
+  theme(legend.position = c(0.8,0.6)) + 
+  geom_hline(yintercept = 102618, color = "grey1")+
+  geom_hline(yintercept = 129266, color = "grey1", linetype = "dashed")
+
+pb.biomass %>% filter(Year <= 2007) %>% summarise(mean(legal))
+pb.biomass %>% filter(Year <= 2007) %>% summarise(mean(mature))
+# gambier ------------
+biomass %>% filter(Location == "Gambier") %>% 
+  select(Year, legal, mature) ->gb.biomass
+gb.biomass_long <- gather(gb.biomass, type, pounds, legal:mature, factor_key = TRUE)
+
+gb <- ggplot(gb.biomass_long, aes(Year, pounds, group = type))+ 
+  geom_point(aes(color = type, shape = type), size =3) +
+  geom_line(aes(color = type, group = type))+
+  scale_colour_manual(name = "", values = c("grey1", "grey1"))+
+  scale_shape_manual(name = "", values = c(16, 1))+
+  
+  ylim(0,125000) +ggtitle("Gambier Bay 2017 Model") + ylab("Biomass (lbs)")+ xlab("Year")+
+  theme(plot.title = element_text(hjust =0.5)) + 
+  scale_x_continuous(breaks = seq(min(1993),max(2017), by =2)) +
+  theme(legend.position = c(0.8,0.6)) + 
+  geom_hline(yintercept = 44763, color = "grey1")+
+  geom_hline(yintercept = 62684, color = "grey1", linetype = "dashed")
+
+gb.biomass %>% filter(Year <= 2007) %>% summarise(mean(legal))
+gb.biomass %>% filter(Year <= 2007) %>% summarise(mean(mature))
+
+# Seymour  -----------
+biomass %>% filter(Location == "Seymour") %>% 
+  select(Year, legal, mature) ->sc.biomass
+sc.biomass_long <- gather(sc.biomass, type, pounds, legal:mature, factor_key = TRUE)
+
+ggplot(sc.biomass_long, aes(Year, pounds, group = type))+ 
+  geom_point(aes(color = type, shape = type), size =3) +
+  geom_line(aes(color = type, group = type))+
+  scale_colour_manual(name = "", values = c("grey1", "grey1"))+
+  scale_shape_manual(name = "", values = c(16, 1))+
+  
+  ylim(0,600000) +ggtitle("Seymour Canal 2017 Model") + ylab("Biomass (lbs)")+ xlab("Year")+
+  theme(plot.title = element_text(hjust =0.5)) + 
+  scale_x_continuous(breaks = seq(min(1993),max(2017), by =2)) +
+  theme(legend.position = c(0.8,0.6)) + 
+  geom_hline(yintercept = 119837, color = "grey1")+
+  geom_hline(yintercept = 147620, color = "grey1", linetype = "dashed")
+
+sc.biomass %>% filter(Year <= 2007) %>% summarise(mean(legal))
+sc.biomass %>% filter(Year <= 2007) %>% summarise(mean(mature))
+# excursion ---------------------
 biomass %>% filter(Location == "Excursion") %>% 
-  select(Year, legal.biomass, harvest, mature.biomass) ->ei.biomass
-ei.biomass_long <- gather(ei.biomass, type, pounds, legal.biomass:mature.biomass, factor_key = TRUE)
+  select(Year, legal, mature) ->ei.biomass
+ei.biomass_long <- gather(ei.biomass, type, pounds, legal:mature, factor_key = TRUE)
 
 ggplot(ei.biomass_long, aes(Year, pounds, group = type))+ 
   geom_point(aes(color = type, shape = type), size =3) +
   geom_line(aes(color = type, group = type))+
-  scale_colour_manual(name = "", values = c("grey1", "grey1", "grey1"))+
-  scale_shape_manual(name = "", values = c(16, 1, 4))+
+  scale_colour_manual(name = "", values = c("grey1", "grey1"))+
+  scale_shape_manual(name = "", values = c(16, 1))+
   
-  ylim(0,200000) +ggtitle("") + ylab("Biomass (lbs)")+ xlab("Year")+
+  ylim(0,150000) +ggtitle("Excursion Inlet 2017 Model") + ylab("Biomass (lbs)")+ xlab("Year")+
   theme(plot.title = element_text(hjust =0.5)) + 
   scale_x_continuous(breaks = seq(min(1993),max(2017), by =2)) +
   theme(legend.position = c(0.8,0.6)) + 
-  geom_hline(yintercept = 83351, color = "grey1")+
-  geom_hline(yintercept = 56513, color = "grey62", linetype = "dashed")
+  geom_hline(yintercept = 29095, color = "grey1")+
+  geom_hline(yintercept = 56413, color = "grey1", linetype = "dashed")
 
-ei.biomass %>% filter(Year <= 2007) %>% summarise(mean(legal.biomass))
-ei.biomass %>% filter(Year <= 2007) %>% summarise(mean(mature.biomass))
+ei.biomass %>% filter(Year <= 2007) %>% summarise(mean(legal))
+ei.biomass %>% filter(Year <= 2007) %>% summarise(mean(mature))
