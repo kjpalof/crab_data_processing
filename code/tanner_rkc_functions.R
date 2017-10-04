518c088fed791fdd56ea39aea74ff27281c6350f
Katie Palof <katie.palof@alaska.gov>
2017-10-04 13:54:47 -0800

    functions modified for tanner data from the red king crab survey

---
+++ code/tanner_rkc_functions.R
@@ -0,0 +1,9 @@
+#K.Palof 10-4-17
+
+#Functions for processing Tanner crab data from the red crab survey.  All areas (except NJ) are
+#     included in one file so need to add a grouping variable for Area/Location
+
+## load packages -----------
+library(tidyverse)
+library(weights)
+library(broom)
