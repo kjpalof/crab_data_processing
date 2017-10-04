6b0736a36f2d8a998fd85629d81007811a33bb4f
Katie Palof <katie.palof@alaska.gov>
2017-10-04 14:04:16 -0800

    change to short term function due to variable name changes and difference in Tanner data output - start

--- code/tanner_rkc_functions.R
+++ code/tanner_rkc_functions.R
@@ -7,3 +7,38 @@
 library(tidyverse)
 library(weights)
 library(broom)
+
+### short term function ----------------
+#input is file with last four years of data summarized by pot
+# area
+# year
+short_t <- function(bypot_st, year) {
+  bypot_st_long <- gather(bypot_st, mod_recruit, crab, Juvenile:Small.Females, factor_key = TRUE) 
+  
+  bypot_st_long %>% 
+    group_by(AREA, mod_recruit) %>% 
+    do(fit = lm(crab ~ Year, data = .)) ->short_term
+  
+  short_term %>%
+    tidy(fit) -> short_term_slope
+  
+  short_term %>%
+    glance(fit) ->short_term_out
+  
+  recruit_used <- c("Large.Females",  "Pre_Recruit", "Recruit","Post_Recruit")
+  short_term_out %>%
+    select(recruit.status, r.squared, p.value)->short_term_out2
+  
+  short_term_slope %>%
+    select(recruit.status, term,  estimate) %>%
+    spread(term, estimate) %>% 
+    right_join(short_term_out2)->short_term_results # estimate here is slope from regression
+  short_term_results %>% 
+    rename(slope = Year) -> short_term_results
+  #Now need to add column for significance and score
+  short_term_results %>%
+    mutate(significant = ifelse(p.value < 0.05 & slope > 0, 1,
+                                ifelse(p.value <0.05 & slope <0, -1, 0))) %>%
+    mutate(score = 0.25*significant) -> short_term_results
+  write_csv(short_term_results, paste0('results/redcrab/', area, '/shortterm.csv'))
+}
