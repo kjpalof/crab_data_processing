96d0536022e4f40114d09ae30b6122ca1eef1c52
Katie Palof <katie.palof@alaska.gov>
2017-10-20 12:06:46 -0800

    short term trends in a function. tanner_functions file

--- code/TCcode/TCS_processingCODE.R
+++ code/TCcode/TCS_processingCODE.R
@@ -135,37 +135,6 @@ dat3 %>%
 
 short_t_tanner(dat3, 2017)
 
-dat3_long <- gather(dat3, mod_recruit, crab, Juvenile:Small.Females, factor_key = TRUE) # need the long version for this.
-
-
-#### Need to include weightings here !!!!!!!!!!!!!!!!!!!!!!!!!!!
-dat3_long %>% # doesn't work with dat2 data because there are no 0's for missing data
-  group_by(Location, mod_recruit) %>%
-  do(fit = lm(crab ~ Year, data =.)) -> short_term
-
-short_term %>%
-  tidy(fit) -> short_term_slope
-
-short_term %>%
-  glance(fit) ->short_term_out
-
-recruit_used <- c("Large.Females",  "Pre_Recruit", "Recruit","Post_Recruit")
-short_term_out %>%
-  filter(mod_recruit %in% recruit_used) %>%
-  select(Location, mod_recruit, r.squared, p.value)->short_term_out2
-
-short_term_slope %>%
-  filter(mod_recruit %in% recruit_used, term == 'Year') %>%
-  select(Location, mod_recruit, estimate) %>%
-  right_join(short_term_out2)->short_term_results # estimate here is slope from regression
-#Now need to add column for significance and score
-short_term_results %>%
-  mutate(significant = ifelse(p.value < 0.05 & estimate > 0, 1,
-                              ifelse(p.value <0.05 & estimate <0, -1, 0))) %>%
-  mutate(score = 0.25*significant) -> short_term_results #estimate is slope from regression
-# final results with score - save here
-write.csv(short_term_results, './results/TCS/TCS_shortterm.csv')
-
 dat3_long %>%
   filter(mod_recruit %in% recruit_used) ->st_dat3_long
   
