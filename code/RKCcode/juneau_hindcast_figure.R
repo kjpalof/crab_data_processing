e3a6c7cdbe29cd59a5ae5b1698c78a35901c8c0f
Katie Palof <katie.palof@alaska.gov>
2018-07-17 15:51:29 -0800

    attempts to edit figure output for word.  Still needs some work

--- code/RKCcode/juneau_hindcast_figure.R
+++ code/RKCcode/juneau_hindcast_figure.R
@@ -56,11 +56,11 @@ jnu_rkc_fig1 <- hindcast %>%
   scale_x_continuous(breaks = seq(min(1975),max(2019), by = 5)) +
   geom_hline(yintercept = 298838, color = "grey1")+
   geom_hline(yintercept = 410878, color = "grey44", linetype = "dashed") +
-  theme(legend.position = c(0.065,0.835), legend.title = element_text(size = 8), 
-        legend.text = element_text(size = 7)) +
+  theme(legend.position = c(0.085,0.733), legend.title = element_text(size = 9), 
+        legend.text = element_text(size = 8)) +
   geom_text(data = baseline_mean_18, aes(x = start_yr, y = baseline, label = label), 
-            hjust = 0.25, vjust = -1, nudge_y = 0.05, size = 3.5) +
-  ggsave('./figures/juneau_fig1_2018.png', width = 10.5, height = 7.5)
+            hjust = -0.65, vjust = -1, nudge_y = 0.05, size = 3.5) +
+  ggsave('./figures/juneau_fig1_2018.png', dpi = 800, width = 7.5, height = 5.5)
 
 
 # Figure A1 ---old Figure 1 - move to Appendix --------
@@ -82,11 +82,11 @@ jnu_rkc_annual_fore <- hindcast %>%
   scale_x_continuous(breaks = seq(min(1975),max(2019), by = 5)) +
   geom_hline(yintercept = 317238, color = "grey1")+
   geom_hline(yintercept = 456142, color = "grey44", linetype = "dashed") +
-  theme(legend.position = c(0.065,0.835), legend.title = element_text(size = 8), 
-        legend.text = element_text(size = 7)) +
+  theme(legend.position = c(0.095,0.738), legend.title = element_text(size = 9), 
+        legend.text = element_text(size = 8)) +
   geom_text(data = baseline_mean_forecast, aes(x = start_yr, y = baseline, label = label), 
-            hjust = 0.25, vjust = -1, nudge_y = 0.05, size = 3.5) +
-  ggsave('./figures/juneau_figA1_2018_test.png', width = 10.5, height = 7.5)
+            hjust = -0.80, vjust = -1, nudge_y = 0.05, size = 3.5) +
+  ggsave('./figures/juneau_figA1_2018.png', dpi = 800, width = 7.5, height = 5.5)
 
 
 #  select(year, legal_2018)figure of 2018 model with forecast in each year -----
