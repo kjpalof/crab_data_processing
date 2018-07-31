b633f14d713201ead9e05ca6e5f28896b9ee0a58
Katie Palof <katie.palof@alaska.gov>
2018-07-31 13:32:06 -0800

    biomass manipulations for figure. Changes to appearance of figures

--- code/RKCcode/panel_figures_wip.R
+++ code/RKCcode/panel_figures_wip.R
@@ -105,14 +105,17 @@ mr_adjust %>%
 
 biomass %>% 
   left_join(mr_adjust2) %>% 
-  mutate(adj.legal = legal.biomass*weighted_ADJ)
-  
-  
-  filter(Location == "Excursion") %>% 
-  select(Year, legal.biomass, harvest, mr.biomass, adj.biomass) ->ei.biomass
-ei.biomass_long <- gather(ei.biomass, type, pounds, legal.biomass:adj.biomass, factor_key = TRUE)
-ei.biomass %>% filter(Year <= 2007) %>% summarise(mean(legal.biomass))
-ei.biomass %>% filter(Year <= 2007) %>% summarise(mean(adj.biomass))
+  mutate(adj.legal = legal.biomass*weighted_ADJ) -> biomass
+
+biomass %>% 
+  select(-weighted_ADJ) %>% 
+  gather(type, pounds, harvest:adj.legal, factor_key = TRUE) %>% 
+  filter(Location == survey.location) -> biomass_graph
+
+biomass_graph %>% 
+  filter(Year <= 2007) %>% 
+  spread(type, pounds) %>% 
+  summarise(legal_mean = mean(legal.biomass), legal_adj_mean = mean(adj.legal)) -> baseline_means
 
 # Figure panel -----
 #### F1a mature male plot -----------
@@ -168,19 +171,22 @@ p3 <- ggplot(female_egg_graph, aes(Year, mean)) +
   theme(legend.position = c(0.2,0.5)) 
 
 ### biomass harvest graph --------------
-
-p4 <- ggplot(ei.biomass_long, aes(Year, pounds, group = type))+ 
+#p4 <- 
+ggplot(biomass_graph, aes(Year, pounds, group = type))+ 
   geom_point(aes(color = type, shape = type), size =3) +
-  geom_line(aes(color = type, group = type))+
-  scale_colour_manual(name = "", values = c("grey1", "grey1", "grey1", "grey62"))+
-  scale_shape_manual(name = "", values = c(16, 1, 18, 18))+
-  
-  ylim(0,715000) +ggtitle("") + ylab("Legal biomass (lbs)")+ xlab("Year")+
+  geom_line(aes(color = type, group = type, linetype = type))+
+  scale_colour_manual(name = "", values = c("grey1", "grey1", "grey48", "grey62"))+
+  scale_shape_manual(name = "", values = c(1, 18, 32, 18))+
+  scale_linetype_manual(name = "", values = c("blank", "solid", "solid", "dashed")) +
+  ylim(0,200000) +
+  ggtitle("") + 
+  ylab("Legal biomass (lbs)") + 
+  xlab("Year") +
   theme(plot.title = element_text(hjust =0.5)) + 
   scale_x_continuous(breaks = seq(min(1993),max(2017), by =2)) +
   theme(legend.position = c(0.8,0.6)) + 
-  geom_hline(yintercept = 83351, color = "grey1")+
-  geom_hline(yintercept = 411756, color = "grey62", linetype = "dashed")
+  geom_hline(data = baseline_means, aes(yintercept = legal_mean), color = "grey1")+
+  geom_hline(data = baseline_means, aes(yintercept = legal_adj_mean), color = "grey62", linetype = "dashed")
 
 
 ### FINAL plot -------------
