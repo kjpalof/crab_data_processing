c90275cece176b7f63ad6b8c021540a3f2a8095c
Katie Palof <katie.palof@alaska.gov>
2018-08-02 16:29:41 -0800

    add code for panel figure

--- code/RKCcode/PSprocessingCODE.R
+++ code/RKCcode/PSprocessingCODE.R
@@ -277,6 +277,51 @@ dat5 %>% group_by(Year, Location) %>%  select(Year, Location, Juvenile, Small.Fe
 write.csv(raw_samp, paste0('./results/redcrab/', survey.location, '/', cur_yr, '/raw_sample.csv'))
 dat5 %>% group_by(Year) %>% summarise (n=n())
 
+### stock assessment figures --------------
+head(CPUE_ALL_YEARS)
+CPUE_ALL_YEARS %>%
+  group_by(Year) %>%
+  summarise(Pre_Recruit_wt = wt.mean(Pre_Recruit, weighting), PreR_SE = (wt.sd(Pre_Recruit, weighting)/(sqrt(sum(!is.na(Pre_Recruit))))), 
+            Recruit_wt = wt.mean(Recruit, weighting), Rec_SE = (wt.sd(Recruit, weighting)/(sqrt(sum(!is.na(Recruit))))), 
+            Post_Recruit_wt = wt.mean(Post_Recruit, weighting), PR_SE = (wt.sd(Post_Recruit, weighting)/(sqrt(sum(!is.na(Post_Recruit))))),
+            Juvenile_wt = wt.mean(Juvenile, weighting), Juv_SE = (wt.sd(Juvenile, weighting)/(sqrt(sum(!is.na(Juvenile))))), 
+            MatF_wt = wt.mean(Large.Females, weighting), MatF_SE = (wt.sd(Large.Females, weighting)/(sqrt(sum(!is.na(Large.Females))))),
+            SmallF_wt = wt.mean(Small.Females, weighting), SmallF_SE = (wt.sd(Small.Females, weighting)/(sqrt(sum(!is.na(Small.Females)))))) -> CPUE_wt_all
+CPUE_wt_all  
+CPUE_wt_all %>% filter(Year >= 1993) -> CPUE_wt_from93
+
+write.csv(CPUE_wt_from93, paste0('results/redcrab/', survey.location, '/', 
+                                 cur_yr, '/cpue_wt_all_yrs.csv'), row.names = FALSE)
+
+
+panel_figure('LynnSisters', 2018, 'LynnSisters')
+
+# panel_figure <- function(survey.location, cur_yr, base.location)
+# base.location is the location name in the baseline file, can be different
+
+### female file all years -----
+# create females file for all years
+# raw_data has OceanAK output until 2016. 
+
+levels(raw_data$Pot.Condition)
+raw_data %>%
+  filter(Pot.Condition == "Normal"|Pot.Condition == "Not observed") -> raw_dat1
+raw_dat1 %>%
+  filter(Sex.Code == 2, Recruit.Status == 'Large Females') -> all_LgF_dat1
+all_LgF_dat1[is.na(all_LgF_dat1$Egg.Percent),]
+all_LgF_dat1 %>% 
+  select(Year, Project.Code, Trip.No, Location, Pot.No, Number.Of.Specimens, 
+         Recruit.Status, Sex.Code, Length.Millimeters, Egg.Percent, 
+         Egg.Development.Code, Egg.Condition.Code)-> LgF_dat1_all
+
+LgF_dat1 %>% 
+  select(Year, Project.Code, Trip.No, Location, Pot.No, Number.Of.Specimens, 
+         Recruit.Status, Sex.Code, Length.Millimeters, Egg.Percent, 
+         Egg.Development.Code, Egg.Condition.Code)-> LgF_dat1_last2
+
+largef_all <- rbind(LgF_dat1_all, LgF_dat1_last2) # raw female data for all years.
+write.csv(largef_all, (paste0('./results/redcrab/', survey.location, '/', cur_yr, '/', 
+                              'largef_all.csv')))
 
 
 
