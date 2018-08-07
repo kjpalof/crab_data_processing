36ecbb57ff248687094795246a771d77c48715eb
Katie Palof <katie.palof@alaska.gov>
2018-08-07 09:58:43 -0800

    regional biomass figure, like figure 2 in old memos.

--- code/RKCcode/figure2_tables_2018.R
+++ code/RKCcode/figure2_tables_2018.R
@@ -37,7 +37,10 @@ regional.b %>%
 regional.b %>% 
   filter(Year >= 1993 & Year <= 2007) %>% 
   summarise(legal_baseline = mean(legal), mature_baseline = mean(mature)) %>% 
-  as.data.frame()
+  as.data.frame() %>% 
+  gather(type, pounds, factor_key = TRUE) %>% 
+  mutate(st_yr = 2007, label = c("Legal (1993-2007)", "Mature (1993-2007)")) ->reg_baseline
+
 # for graphing
 #regional.b %>% 
 #  gather(type, pounds, legal:mature, factor_key = TRUE) -> regional.
@@ -49,29 +52,34 @@ regional.b %>%
 regional.b %>% 
   gather(type, pounds, legal:mature, factor_key = TRUE) %>% 
   ggplot(aes(Year, pounds, group = type)) +
-  geom_point(aes(color = type, shape = status, fill = type), size =3) +
-  geom_line(aes(color = type, group = type, linetype = type))+
-  scale_colour_manual(name = "", values = c("black", "grey44"))+
+  geom_line(aes(colour = type, group = type, linetype = type))+
+  geom_point(aes(colour = type, shape = status, fill = type), size =3) +
+  geom_hline(data = reg_baseline, aes(yintercept = pounds, linetype = type, colour = type)) +
+  scale_colour_manual(name = "", values = c("black", "black", "grey60", "grey60"), 
+                      guide = FALSE)+
   scale_shape_manual(name = "Fishery Status", values = c(25, 21, 8))+
-  scale_linetype_manual(name = "", values = c("solid", "dashed")) +
-  scale_fill_manual(name = "", values = c("black", "grey44")) +
-  scale_y_continuous(labels = comma, limits = c(0,max(regional.b$mature,
-                                                na.rm = TRUE)),
-                     breaks= seq(min(0), max(max(regional.b$mature, na.rm = TRUE)), 
-                                 by = 100000)) +
+  scale_linetype_manual(name = "", values = c("solid", "solid", "dashed", "dashed"), 
+                        guide = FALSE) +
+  scale_fill_manual(name = "", values = c("black", "gray75"), 
+                    guide = FALSE) +
+  scale_y_continuous(labels = comma, limits = c(0,(max(regional.b$mature,
+                                                na.rm = TRUE) + 100000)),
+                     breaks= seq(min(0), max(max(regional.b$mature, na.rm = TRUE) +100000), 
+                                 by = 200000)) +
   scale_x_continuous(breaks = seq(min(1975),max(max(regional.b$Year) + 1), by = 2)) +
   ggtitle("Biomass of surveyed areas for Southeast Alaska red king crab") + 
   ylab("Biomass (lb)") + 
   theme(plot.title = element_text(hjust =0.5)) +
   theme(legend.position = c(0.825,0.793), legend.title = element_text(size = 9), 
-      legend.text = element_text(size = 8)) 
-  
+      legend.text = element_text(size = 8), axis.text.x = element_text(angle = 45)) +
+  theme(axis.text.x = element_text(vjust = 0.50)) +
+  geom_text(data = reg_baseline, aes(x = st_yr, y = pounds, label = label), 
+          hjust = -0.05, vjust = 1.5, nudge_y = 0.05, size = 3.5) +
+  ggsave(paste0('./figures/redcrab/regional_biomass', cur_yr, '.png'), dpi = 800, width = 7.5, height = 5.5)
+
+
   #ggtitle("Juneau 2018 model") + ylab("Estimated Biomass (lbs)")+ xlab("Year")+
   geom_hline(yintercept = 298838, color = "grey1")+
   geom_hline(yintercept = 410878, color = "grey44", linetype = "dashed") +
-  theme(legend.position = c(0.125,0.793), legend.title = element_text(size = 9), 
-        legend.text = element_text(size = 8)) +
-  geom_text(data = baseline_mean_18, aes(x = start_yr, y = baseline, label = label), 
-            hjust = -0.45, vjust = 1.5, nudge_y = 0.05, size = 3.5) +
   guides(shape = guide_legend(ncol=2), group = guide_legend((ncol =2))) +
-  ggsave('./figures/juneau_fig1_2018.png', dpi = 800, width = 7.5, height = 5.5)
+  
