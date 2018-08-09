c1575b361f0031f123c618ea409b2e887183d345
Katie Palof <katie.palof@alaska.gov>
2018-08-09 15:59:35 -0800

    create file for females to use in upcoming years

--- code/RKCcode/SCprocessingCODE.R
+++ code/RKCcode/SCprocessingCODE.R
@@ -312,7 +312,29 @@ panel_figure('Seymour', 2018, 'Seymour', 3) # female panel
 # base.location is the location name in the baseline file, can be different
 
 
+### female file all years -----
+# create females file for all years
+# raw_data has OceanAK output until 2016. 
 
+levels(raw_data$Pot.Condition)
+raw_data %>%
+  filter(Pot.Condition == "Normal"|Pot.Condition == "Not observed") -> raw_dat1
+raw_dat1 %>%
+  filter(Sex.Code == 2, Recruit.Status == 'Large Females') -> all_LgF_dat1
+all_LgF_dat1[is.na(all_LgF_dat1$Egg.Percent),]
+all_LgF_dat1 %>% 
+  select(Year, Project.Code, Trip.No, Location, Pot.No, Number.Of.Specimens, 
+         Recruit.Status, Sex.Code, Length.Millimeters, Egg.Percent, 
+         Egg.Development.Code, Egg.Condition.Code)-> LgF_dat1_all
+
+LgF_dat1 %>% 
+  select(Year, Project.Code, Trip.No, Location, Pot.No, Number.Of.Specimens, 
+         Recruit.Status, Sex.Code, Length.Millimeters, Egg.Percent, 
+         Egg.Development.Code, Egg.Condition.Code)-> LgF_dat1_last2
+
+largef_all <- rbind(LgF_dat1_all, LgF_dat1_last2) # raw female data for all years.
+write.csv(largef_all, (paste0('./results/redcrab/', survey.location, '/', cur_yr, '/', 
+                              'largef_all.csv')))
 
 
 
