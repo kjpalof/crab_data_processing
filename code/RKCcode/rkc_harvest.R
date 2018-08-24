cb13cde070a1333aa7de141484744cd974ba89f0
Katie Palof <katie.palof@alaska.gov>
2018-08-23 16:36:39 -0800

    issues with differences in fish tickets from now - august to earlier pull

--- code/RKCcode/rkc_harvest.R
+++ code/RKCcode/rkc_harvest.R
@@ -11,18 +11,28 @@ library(xlsx)
 
 #####Load Data ---------------------------------------------------
 # change input file and input folder for each
-harvest <- read.csv("./data/redcrab/RKC_fish_tickets.csv")
+harvest <- read.csv("./data/redcrab/RKC_fish_tickets_2017.csv") #
+# data pulled from OceanAK August 23rd, 2018
+#harvest_A <- read.csv("./data/redcrab/RKC_Detailed Fish Tickets_aug23.csv") 
+# from OceanAK detailed fish tickets 
 glimpse(harvest)
 survey.area <- read.xlsx('data/redcrab/rkc_biomass_2017_model.xlsx', sheetName = "Sheet2") #stat area to survey area conversion
 personal_use <- read.csv("./data/redcrab/personal_use_RKC_11A_2017.csv")
   
 ### clean up  ------
 unique(harvest$Species.Code.and.Name)
+
 harvest %>% 
   select(Species.Class, Season, CFEC, ADFG.Number, Date.of.Landing, Stat.Area, 
          Species.Code.and.Name, Number.Of.Animals, Whole.Weight..sum., Pot.Lifts) %>%  
   rename(stat.area = Stat.Area) %>% 
   left_join(survey.area) -> harvest2
+
+harvest_A %>% 
+  select(Species.Class, Season, CFEC, ADFG.Number, Date.of.Landing, Stat.Area, 
+         Species.Code.and.Name, Number.Of.Animals, Whole.Weight..sum., Pot.Lifts) %>%  
+  rename(stat.area = Stat.Area) %>% 
+  left_join(survey.area) -> harvest2_A
 
 # by stat area ----
 harvest2 %>% 
@@ -31,6 +41,12 @@ harvest2 %>%
             numbers = sum(Number.Of.Animals), 
             pounds = sum(Whole.Weight..sum.), 
             pots = sum(Pot.Lifts, na.rm = TRUE)) -> catch_by_stat
+harvest2_A %>% 
+  group_by(Season, stat.area) %>% 
+  summarise(permits = length(unique(CFEC)), 
+            numbers = sum(Number.Of.Animals), 
+            pounds = sum(Whole.Weight..sum.), 
+            pots = sum(Pot.Lifts, na.rm = TRUE)) -> catch_by_stat_A
 
 # by stat area and survey area ----
 harvest2 %>% 
@@ -40,6 +56,12 @@ harvest2 %>%
             pounds = sum(Whole.Weight..sum.), 
             pots = sum(Pot.Lifts, na.rm = TRUE)) -> catch_by_stat_survey
 
+harvest2_A %>% 
+  group_by(Season, stat.area, survey.area) %>% 
+  summarise(permits = length(unique(CFEC)), 
+            numbers = sum(Number.Of.Animals), 
+            pounds = sum(Whole.Weight..sum.), 
+            pots = sum(Pot.Lifts, na.rm = TRUE)) -> catch_by_stat_survey_A
 # by survey area ----
 harvest2 %>% 
   group_by(Season, survey.area) %>% 
@@ -47,12 +69,22 @@ harvest2 %>%
             numbers = sum(Number.Of.Animals), 
             pounds = sum(Whole.Weight..sum.), 
             pots = sum(Pot.Lifts, na.rm = TRUE)) %>% 
-  mutate(avg_wt = pounds/numbers) -> catch_by_survey
-
+  mutate(avg_wt = pounds/numbers, 
+         proportion = pounds/(sum(pounds))) -> catch_by_survey
+harvest2_A %>% 
+  group_by(Season, survey.area) %>% 
+  summarise(permits = length(unique(CFEC)), 
+            numbers = sum(Number.Of.Animals), 
+            pounds = sum(Whole.Weight..sum.), 
+            pots = sum(Pot.Lifts, na.rm = TRUE)) %>% 
+  mutate(avg_wt = pounds/numbers, 
+         proportion = pounds/(sum(pounds))) -> catch_by_survey_A
 
 write.csv(catch_by_stat, './results/redcrab/comm_catch_by_statarea.csv')
 write.csv(catch_by_survey, './results/redcrab/comm_catch_by_surveyarea.csv')
 write.csv(catch_by_stat_survey, './results/redcrab/comm_catch_by_stat_and_surveyarea.csv')
+
+
 ### mid-catch date ------------------
 harvest2 %>%
   group_by(survey.area, Date.of.Landing) %>%
