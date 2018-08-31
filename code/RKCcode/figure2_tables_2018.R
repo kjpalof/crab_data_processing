c17f4c2de947ebdd0907d73c3ef64e185dd77bdf
Katie Palof <katie.palof@alaska.gov>
2018-08-31 13:24:11 -0800

    redo figure 2 with adjusted biomass estimates to follow document. Also create .csv for appendix tables

--- code/RKCcode/figure2_tables_2018.R
+++ code/RKCcode/figure2_tables_2018.R
@@ -22,10 +22,25 @@ biomass <- read.csv("./data/redcrab/biomass.csv")
 exploit_rate <- read.csv("./data/redcrab/table3.csv")
 
 ## clean up figure 2-------
+# add in mr adjustments
+mr_adjust %>% 
+  select(-X) %>% 
+  mutate(Location = ifelse(area == "St_James", "LynnSisters", as.character(area))) %>% 
+  select(-area) -> mr_adjust2
+biomass %>% 
+  left_join(mr_adjust2) %>% 
+  mutate(adj.legal = ifelse(Location == "Juneau", legal.biomass, 
+                            legal.biomass*weighted_ADJ), 
+         adj.mature = ifelse(Location == "Juneau", mature.biomass, 
+                            mature.biomass*weighted_ADJ))-> biomass
+write.csv(biomass, paste0('./results/redcrab/biomass_', cur_yr, '.csv'))
+# use these values for Table A2 in stock health document
+
 # regional biomass ----
 biomass %>% 
   group_by(Year) %>% 
-  summarise(legal = sum(legal.biomass), mature = sum(mature.biomass)) %>% 
+  summarise(legal = sum(legal.biomass), mature = sum(mature.biomass), 
+            adj_legal = sum(adj.legal), adj_mature = sum(adj.mature)) %>% 
   as.data.frame() -> regional.b
 fishery.status %>% 
   select(Year = year, status) %>% 
@@ -33,7 +48,7 @@ fishery.status %>%
 
 regional.b %>% 
   left_join(fishery.status) -> regional.b
-
+write.csv(regional.b, paste0('./results/redcrab/regional_biomass_', cur_yr, '.csv'))
 # change in biomass estimation ----
 regional.b %>% 
   filter(Year > cur_yr-2) %>% 
@@ -46,25 +61,30 @@ regional.b %>%
 # 1993 - 2007 
 regional.b %>% 
   filter(Year >= 1993 & Year <= 2007) %>% 
-  summarise(legal_baseline = mean(legal), mature_baseline = mean(mature)) %>% 
+  summarise(legal_baseline = mean(legal), mature_baseline = mean(mature), 
+            adj.legal_base = mean(adj_legal), adj.mature.base = mean(adj_mature)) %>% 
   as.data.frame() %>% 
   gather(type, pounds, factor_key = TRUE) %>% 
-  mutate(st_yr = 2007, label = c("Legal (1993-2007)", "Mature (1993-2007)")) ->reg_baseline
+  mutate(st_yr = 2007, label = c("CSA Legal (1993-2007)", "CSA Mature (1993-2007)", 
+                                 "Legal (1993-2007)", "Mature (1993-2007)")) ->reg_baseline
 
+reg_baseline[1:2, ] ->  reg_baseline_CSA
+reg_baseline[3:4, ] ->  reg_baseline_MR
 # for graphing
 #regional.b %>% 
 #  gather(type, pounds, legal:mature, factor_key = TRUE) -> regional.
 
 
-# Figure 2 regional biomass ---------
+# Figure 2 regional biomass CSA biomass---------
 # should have 2018 model with longterm baselines (1993-2007) and closure status. 
 #   also show 2018 forecast as distinct from model output
 regional.b %>% 
+  select(Year, legal, mature, status) %>% 
   gather(type, pounds, legal:mature, factor_key = TRUE) %>% 
   ggplot(aes(Year, pounds, group = type)) +
   geom_line(aes(colour = type, group = type, linetype = type))+
   geom_point(aes(colour = type, shape = status, fill = type), size =3) +
-  geom_hline(data = reg_baseline, aes(yintercept = pounds, linetype = type, colour = type)) +
+  geom_hline(data = reg_baseline_CSA, aes(yintercept = pounds, linetype = type, colour = type)) +
   scale_colour_manual(name = "", values = c("black", "black", "grey60", "grey60"), 
                       guide = FALSE)+
   scale_shape_manual(name = "Fishery Status", values = c(25, 21, 8))+
@@ -77,7 +97,7 @@ regional.b %>%
                      breaks= seq(min(0), max(max(regional.b$mature, na.rm = TRUE) +100000), 
                                  by = 200000)) +
   scale_x_continuous(breaks = seq(min(1975),max(max(regional.b$Year) + 1), by = 2)) +
-  ggtitle("Biomass of surveyed areas for Southeast Alaska red king crab") + 
+  ggtitle("CSA biomass of surveyed areas for Southeast Alaska red king crab") + 
   ylab("Biomass (lb)") + 
   theme(plot.title = element_text(hjust =0.5)) +
   theme(legend.position = c(0.825,0.793), legend.title = element_text(size = 9), 
@@ -85,12 +105,47 @@ regional.b %>%
       axis.title = element_text(size = 14, face = "bold"), 
       axis.text = element_text(size = 12)) +
   theme(axis.text.x = element_text(vjust = 0.50)) +
-  geom_text(data = reg_baseline, aes(x = st_yr, y = pounds, label = label), 
+  geom_text(data = reg_baseline_CSA, aes(x = st_yr, y = pounds, label = label), 
           hjust = -0.05, vjust = 1.5, nudge_y = 0.05, size = 3.5) +
-  ggsave(paste0('./figures/redcrab/regional_biomass', cur_yr, '.png'), dpi = 800, width = 7.5, height = 5.5)
+  ggsave(paste0('./figures/redcrab/CSAregional_biomass', cur_yr, '.png'), dpi = 800, width = 7.5, height = 5.5)
 
 
-  
+# Figure 2 regional biomass M/R adjusted biomass---------
+# should have 2018 model with longterm baselines (1993-2007) and closure status. 
+#   also show 2018 forecast as distinct from model output
+regional.b %>% 
+  select(Year, adj_legal, adj_mature, status) %>%
+  gather(type, pounds, adj_legal:adj_mature, factor_key = TRUE) %>% 
+  ggplot(aes(Year, pounds, group = type)) +
+  geom_line(aes(colour = type, group = type, linetype = type))+
+  geom_point(aes(colour = type, shape = status, fill = type), size =3) +
+  geom_hline(data = reg_baseline_MR, aes(yintercept = pounds, 
+                                         linetype = type, colour = type)) +
+  scale_colour_manual(name = "", values = c("black", "grey60", "black", "grey60"), 
+                      guide = FALSE)+
+  scale_shape_manual(name = "Fishery Status", values = c(25, 21, 8))+
+  scale_linetype_manual(name = "", values = c("solid", "dashed", "solid", "dashed"), 
+                        guide = FALSE) +
+  scale_fill_manual(name = "", values = c("black", "gray75"), 
+                    guide = FALSE) +
+  scale_y_continuous(labels = comma, limits = c(0,(max(regional.b$adj_mature,
+                                                       na.rm = TRUE) + 100000)),
+                     breaks= seq(min(0), max(max(regional.b$adj_mature, na.rm = TRUE) +100000), 
+                                 by = 500000)) +
+  scale_x_continuous(breaks = seq(min(1975),max(max(regional.b$Year) + 1), by = 2)) +
+  ggtitle("Biomass of surveyed areas for Southeast Alaska red king crab") + 
+  ylab("Biomass (lb)") + 
+  theme(plot.title = element_text(hjust =0.5)) +
+  theme(legend.position = c(0.825,0.793), legend.title = element_text(size = 9), 
+        legend.text = element_text(size = 8), axis.text.x = element_text(angle = 45), 
+        axis.title = element_text(size = 14, face = "bold"), 
+        axis.text = element_text(size = 12)) +
+  theme(axis.text.x = element_text(vjust = 0.50)) +
+  geom_text(data = reg_baseline_MR, aes(x = st_yr, y = pounds, label = label), 
+            hjust = -0.05, vjust = 1.5, nudge_y = 0.05, size = 3.5) +
+  ggsave(paste0('./figures/redcrab/MRregional_biomass', cur_yr, '.png'), dpi = 800, width = 7.5, height = 5.5)
+
+
 
 
 
