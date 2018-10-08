416359c1d971c374a124cd7d24608933ae127f5c
Katie Palof <katie.palof@alaska.gov>
2018-10-08 14:37:29 -0800

    code to process all years of harvest since 1997-1998 season

--- code/tanner_harvest.R
+++ code/tanner_harvest.R
@@ -14,6 +14,9 @@ cur_yr = 2018
 harvest <- read.csv("./data/Tanner_Detailed Fish Tickets.csv")
 glimpse(harvest)
 
+harvest_all <- read.csv("./data/Tanner_Detailed Fish Tickets_98_18.csv")
+
+### current year ----------------------
 unique(harvest$Stat.Area)
 # need to create column that does what 'Survey area 3' does in Excel sheet
 # refer to '2014-2015 fish tickets.xlsx'
@@ -52,7 +55,7 @@ write.csv(harvest2, paste0('./results/tanner/comm_catch_by_statarea', cur_yr,'.c
 #  filter(Stat.Area == 11510, Season == 'Sep2015 - Aug16') %>%
 #  select(Season, CFEC, Stat.Area, )
 
-### by survey area --------------------------
+### current year by survey area --------------------------
 harvest %>%
   filter(Date.of.Landing != '2018-07-13 00:00:00') %>% 
   group_by(Season, survey.area)%>%
@@ -61,7 +64,7 @@ harvest %>%
 
 # lynn sister and north juneau need to be manually split up in area 115-10
 write.csv(comm.catch.sum, paste0('./results/tanner/tanner_comm_catch', cur_yr,'.csv'))
-### mid-catch date ------------------
+### current year mid-catch date ------------------
 harvest %>%
   #filter (Season == "Sep2017 - Aug18") %>% 
   filter(Date.of.Landing != '2018-07-13 00:00:00') %>% 
@@ -77,9 +80,56 @@ mid.catch %>%
 
 write.csv(mid.catch2, paste0('./results/tanner/tanner_mid_catch_date', cur_yr, '.csv'))
 
-### total annual harvest  ---------------------
+### current year total annual harvest  ---------------------
 comm.catch.sum %>%
   group_by(Season)%>%
   summarise(numbers = sum(numbers), pounds = sum(pounds)) -> annual_catch
 
-write.csv(annual_catch, './results/tanner/tanner_annual_catch_17.csv')
+write.csv(annual_catch, paste0('./results/tanner/tanner_annual_catch_', cur_yr,'.csv'))
+
+
+### all years ----------------------
+unique(harvest_all$Stat.Area)
+# need to create column that does what 'Survey area 3' does in Excel sheet
+# refer to '2014-2015 fish tickets.xlsx'
+harvest_all %>%
+  mutate(survey.area = ifelse(Stat.Area ==11023, 'Gambier', ifelse(Stat.Area == 11423, 'Icy', 
+                        ifelse(Stat.Area == 11470, 'Glacier Bay', ifelse(Stat.Area == 11012, 
+                         'Thomas', ifelse(Stat.Area == 11150,'North Juneau', 
+                          ifelse(Stat.Area ==11021 | Stat.Area ==11022, 'Pybus', 
+                           ifelse(Stat.Area == 11480 |Stat.Area ==11425, 'Excursion', 
+                            ifelse(Stat.Area==11120 | Stat.Area ==11121, 'Holkham', 
+                             ifelse(Stat.Area==11140|Stat.Area==11141|Stat.Area==11142|Stat.Area==11143,
+                              'Stephens', 
+                               ifelse(Stat.Area == 11351|Stat.Area == 11352|Stat.Area == 11353|
+                                Stat.Area == 11354|Stat.Area == 11355|Stat.Area == 11356|Stat.Area == 11357|
+                                 Stat.Area == 11358, 'Peril', 
+                           ifelse(Stat.Area == 11101|Stat.Area == 11102|Stat.Area == 11103|
+                                  Stat.Area == 11104|Stat.Area == 11105|Stat.Area == 11106|
+                                  Stat.Area == 11107|Stat.Area == 11108|Stat.Area == 11109|
+                                  Stat.Area == 11110|Stat.Area == 11111|Stat.Area == 11112|
+                                  Stat.Area == 11113|Stat.Area == 11114|Stat.Area == 11115|Stat.Area == 11116|
+                                  Stat.Area == 11117|Stat.Area == 11118, 'Seymour', 
+                           ifelse(Stat.Area == 11431|Stat.Area == 11432|Stat.Area == 11433|Stat.Area == 11434, 
+                                  'PFred', 
+                           ifelse(Stat.Area == 11510|Stat.Area == 11215, 'Lynn', 
+                           ifelse(Stat.Area == 10940|Stat.Area == 10941|Stat.Area == 10942|Stat.Area == 10943|
+                                    Stat.Area ==10532, 'Camden', 'Other'))))))))))))))) -> harvest_all
+# remove 11511 from Lynn Canal - make it part of 'other'
+# by stat area, not needed for this analysis
+harvest_all %>%
+  group_by(Season, Stat.Area, survey.area) %>%
+  summarise(permits = length(unique(CFEC)), 
+            numbers = sum(Number.Of.Animals), 
+            pounds = sum(Whole.Weight..sum.)) -> harvest2_all
+
+write.csv(harvest2_all, paste0('./results/tanner/comm_catch_by_statarea_98_', cur_yr,'.csv'))
+
+### all years by survey area --------------------------
+harvest_all %>%
+  group_by(Season, survey.area)%>%
+  summarise(permits = length(unique(CFEC)), numbers = sum(Number.Of.Animals), 
+            pounds = sum(Whole.Weight..sum.)) -> comm.catch.sum_all
+
+# lynn sister and north juneau need to be manually split up in area 115-10
+write.csv(comm.catch.sum_all, paste0('./results/tanner/tanner_comm_catch_98_', cur_yr,'.csv'))
