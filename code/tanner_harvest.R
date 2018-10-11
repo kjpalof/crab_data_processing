24a883d6c25d74f8fd58f44fb1734b0dc56dc603
Katie Palof <katie.palof@alaska.gov>
2018-10-11 09:51:44 -0800

    change survey area names to be consistent with biomass file. Confirm that NA's are being removed

--- code/tanner_harvest.R
+++ code/tanner_harvest.R
@@ -21,26 +21,28 @@ unique(harvest$Stat.Area)
 # need to create column that does what 'Survey area 3' does in Excel sheet
 # refer to '2014-2015 fish tickets.xlsx'
 harvest %>%
-  mutate(survey.area = ifelse(Stat.Area ==11023, 'Gambier', ifelse(Stat.Area == 11423, 'Icy', 
-                                ifelse(Stat.Area == 11470, 'Glacier Bay', 
-                                   ifelse(Stat.Area == 11012, 'Thomas', ifelse(Stat.Area == 11150,'North Juneau', 
-                                    ifelse(Stat.Area ==11021 | Stat.Area ==11022, 'Pybus', 
-                                     ifelse(Stat.Area == 11480 |Stat.Area ==11425, 'Excursion', 
-                                      ifelse(Stat.Area==11120 | Stat.Area ==11121, 'Holkham', 
-                                       ifelse(Stat.Area==11140|Stat.Area==11141|Stat.Area==11142|Stat.Area==11143,
-                                              'Stephens', 
-                              ifelse(Stat.Area == 11351|Stat.Area == 11352|Stat.Area == 11353|
-                                       Stat.Area == 11354|Stat.Area == 11355|Stat.Area == 11356|Stat.Area == 11357|
-                                       Stat.Area == 11358, 'Peril', 
-                                              ifelse(Stat.Area == 11101|Stat.Area == 11102|Stat.Area == 11103|Stat.Area == 11104|
-                                                  Stat.Area == 11105|Stat.Area == 11106|Stat.Area == 11107|Stat.Area == 11108|
-                                                    Stat.Area == 11109|Stat.Area == 11110|Stat.Area == 11111|Stat.Area == 11112|
-                                                    Stat.Area == 11113|Stat.Area == 11114|Stat.Area == 11115|Stat.Area == 11116|
-                                                    Stat.Area == 11117|Stat.Area == 11118, 'Seymour', 
-                                          ifelse(Stat.Area == 11431|Stat.Area == 11432|Stat.Area == 11433|Stat.Area == 11434, 'PFred', 
-                                                  ifelse(Stat.Area == 11510|Stat.Area == 11215, 'Lynn', 
-                                              ifelse(Stat.Area == 10940|Stat.Area == 10941|Stat.Area == 10942|Stat.Area == 10943|Stat.Area ==10532,
-                                                           'Camden', 'Other'))))))))))))))) -> harvest
+  mutate(survey.area = ifelse(Stat.Area ==11023, 'Gambier Bay', ifelse(Stat.Area == 11423, 'Icy Strait', 
+                        ifelse(Stat.Area == 11470, 'Glacier Bay', ifelse(Stat.Area == 11012, 
+                        'Thomas Bay', ifelse(Stat.Area == 11150,'North Juneau', 
+                         ifelse(Stat.Area ==11021 | Stat.Area ==11022, 'Pybus Bay', 
+                         ifelse(Stat.Area == 11480 |Stat.Area ==11425, 'Excursion Inlet', 
+                         ifelse(Stat.Area==11120 | Stat.Area ==11121, 'Holkham Bay', 
+                         ifelse(Stat.Area==11140|Stat.Area==11141|Stat.Area==11142|Stat.Area==11143,
+                             'Stephens Passage', 
+                         ifelse(Stat.Area == 11351|Stat.Area == 11352|Stat.Area == 11353|
+                         Stat.Area == 11354|Stat.Area == 11355|Stat.Area == 11356|Stat.Area == 11357|
+                         Stat.Area == 11358, 'Peril Strait', 
+                         ifelse(Stat.Area == 11101|Stat.Area == 11102|Stat.Area == 11103|
+                         Stat.Area == 11104|Stat.Area == 11105|Stat.Area == 11106|
+                         Stat.Area == 11107|Stat.Area == 11108|Stat.Area == 11109|
+                         Stat.Area == 11110|Stat.Area == 11111|Stat.Area == 11112|
+                         Stat.Area == 11113|Stat.Area == 11114|Stat.Area == 11115|Stat.Area == 11116|
+                         Stat.Area == 11117|Stat.Area == 11118, 'Seymour Canal', 
+                         ifelse(Stat.Area == 11431|Stat.Area == 11432|Stat.Area == 11433|Stat.Area == 11434, 
+                            'PFred', 
+                         ifelse(Stat.Area == 11510|Stat.Area == 11215, 'Lynn Sisters', 
+                         ifelse(Stat.Area == 10940|Stat.Area == 10941|Stat.Area == 10942|Stat.Area == 10943|
+                                Stat.Area ==10532, 'Camden', 'Other')))))))))))))))  -> harvest
 # remove 11511 from Lynn Canal - make it part of 'other'
 # by stat area, not needed for this analysis
 harvest %>%
@@ -93,26 +95,26 @@ unique(harvest_all$Stat.Area)
 # need to create column that does what 'Survey area 3' does in Excel sheet
 # refer to '2014-2015 fish tickets.xlsx'
 harvest_all %>%
-  mutate(survey.area = ifelse(Stat.Area ==11023, 'Gambier', ifelse(Stat.Area == 11423, 'Icy', 
+  mutate(survey.area = ifelse(Stat.Area ==11023, 'Gambier Bay', ifelse(Stat.Area == 11423, 'Icy Strait', 
                         ifelse(Stat.Area == 11470, 'Glacier Bay', ifelse(Stat.Area == 11012, 
-                         'Thomas', ifelse(Stat.Area == 11150,'North Juneau', 
-                          ifelse(Stat.Area ==11021 | Stat.Area ==11022, 'Pybus', 
-                           ifelse(Stat.Area == 11480 |Stat.Area ==11425, 'Excursion', 
-                            ifelse(Stat.Area==11120 | Stat.Area ==11121, 'Holkham', 
+                         'Thomas Bay', ifelse(Stat.Area == 11150,'North Juneau', 
+                          ifelse(Stat.Area ==11021 | Stat.Area ==11022, 'Pybus Bay', 
+                           ifelse(Stat.Area == 11480 |Stat.Area ==11425, 'Excursion Inlet', 
+                            ifelse(Stat.Area==11120 | Stat.Area ==11121, 'Holkham Bay', 
                              ifelse(Stat.Area==11140|Stat.Area==11141|Stat.Area==11142|Stat.Area==11143,
-                              'Stephens', 
+                              'Stephens Passage', 
                                ifelse(Stat.Area == 11351|Stat.Area == 11352|Stat.Area == 11353|
                                 Stat.Area == 11354|Stat.Area == 11355|Stat.Area == 11356|Stat.Area == 11357|
-                                 Stat.Area == 11358, 'Peril', 
+                                 Stat.Area == 11358, 'Peril Strait', 
                            ifelse(Stat.Area == 11101|Stat.Area == 11102|Stat.Area == 11103|
                                   Stat.Area == 11104|Stat.Area == 11105|Stat.Area == 11106|
                                   Stat.Area == 11107|Stat.Area == 11108|Stat.Area == 11109|
                                   Stat.Area == 11110|Stat.Area == 11111|Stat.Area == 11112|
                                   Stat.Area == 11113|Stat.Area == 11114|Stat.Area == 11115|Stat.Area == 11116|
-                                  Stat.Area == 11117|Stat.Area == 11118, 'Seymour', 
+                                  Stat.Area == 11117|Stat.Area == 11118, 'Seymour Canal', 
                            ifelse(Stat.Area == 11431|Stat.Area == 11432|Stat.Area == 11433|Stat.Area == 11434, 
                                   'PFred', 
-                           ifelse(Stat.Area == 11510|Stat.Area == 11215, 'Lynn', 
+                           ifelse(Stat.Area == 11510|Stat.Area == 11215, 'Lynn Sisters', 
                            ifelse(Stat.Area == 10940|Stat.Area == 10941|Stat.Area == 10942|Stat.Area == 10943|
                                     Stat.Area ==10532, 'Camden', 'Other'))))))))))))))) -> harvest_all
 # remove 11511 from Lynn Canal - make it part of 'other'
@@ -120,8 +122,8 @@ harvest_all %>%
 harvest_all %>%
   group_by(Season, Stat.Area, survey.area) %>%
   summarise(permits = length(unique(CFEC)), 
-            numbers = sum(Number.Of.Animals), 
-            pounds = sum(Whole.Weight..sum.)) -> harvest2_all
+            numbers = sum(Number.Of.Animals, na.rm = TRUE), 
+            pounds = sum(Whole.Weight..sum., na.rm = TRUE)) -> harvest2_all
 
 write.csv(harvest2_all, paste0('./results/tanner/comm_catch_by_statarea_98_', cur_yr,'.csv'))
 
@@ -138,7 +140,7 @@ numextract <- function(string){
 } 
 
 comm.catch.sum_all %>% 
-  mutate(year = as.numeric(numextract(Season))) -> comm.catch.sum_all
+  mutate(Year = as.numeric(numextract(Season))) -> comm.catch.sum_all
 
 
 # lynn sister and north juneau need to be manually split up in area 115-10
