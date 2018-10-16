fb83ceedbebcfa9c77ea969a31d80e16fe002935
Katie Palof <katie.palof@alaska.gov>
2018-10-16 11:48:53 -0800

    change to pulling logbook data for all years just to look at 115-10 stat area

--- code/tanner_logbook.R
+++ code/tanner_logbook.R
@@ -1,14 +1,16 @@
-# K.Palof 10-6-17
+# K.Palof 10-16-18 updated 
 # Code to review logbook data for Tanner crab fishery.
 # needed to seperate out catch for Lynn Sisters and North Juneau, previously done in .JMP and Excel
 
-# logbook data
-rm(list = ls()) # clear workspace since data frames have same names
+# logbook data from ALEX (as of 10-16-18) pull only data from district 115 - have to sort here for sub-district 10
+#    since that's all that is needed. Pull for all years and save file in results/tanner 
+
+
 #####Load Packages ---------------------------------
 library(tidyverse)
 library(readxl)
 
-#####Load Data ---------------------------------------------------
+#####Load Data -------------------------------------
 # change input file and input folder for each
 logb <- read_excel(path = "./data/TannerLogbookData_2017.xlsx", sheet = 1)
 glimpse(logb)
