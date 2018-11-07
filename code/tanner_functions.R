2a3de161e94033eb688cd90bc6b9b85dd5fe88a8
Katie Palof <katie.palof@alaska.gov>
2018-11-06 16:27:47 -0900

    save to tanner location

--- code/tanner_functions.R
+++ code/tanner_functions.R
@@ -386,7 +386,7 @@ panel_figure <- function(survey.location, cur_yr, area, option){
                 panel <- plot_grid(p1, p4, ncol = 1, align = 'v'), 
                 ifelse(option == 3, 
                        panel <- plot_grid(p2, p3, ncol = 1, align = 'v'), 0)))
-  ggsave(paste0('./figures/rkcs_tanner/', survey.location, '_', cur_yr, '_', 
+  ggsave(paste0('./figures/tanner/', survey.location, '_', cur_yr, '_', 
                 option, '.png'), panel,  
          dpi = 800, width = 8, height = 9.5)
 }
