0440f2cd9b97bf2d013f74b0811f583a1ab6981f
Katie Palof <katie.palof@alaska.gov>
2018-11-08 16:30:12 -0900

    legend position

--- code/tanner_rkc_functions.R
+++ code/tanner_rkc_functions.R
@@ -358,7 +358,7 @@ panel_figure <- function(survey.location, cur_yr, area, option){
                                                         na.rm = TRUE) + 25000),
                        breaks= seq(min(0), max(max(biomass_graph$pounds, 
                                                    na.rm = TRUE)+25000), by = 50000)) +
-    theme(legend.position = c(0.7,0.8), 
+    theme(legend.position = c(0.55,0.8), 
           axis.text = element_text(size = 12), 
           axis.title=element_text(size=14,face="bold")) + 
     geom_hline(data = baseline_means, aes(yintercept = legal_mean), color = "grey1", 
