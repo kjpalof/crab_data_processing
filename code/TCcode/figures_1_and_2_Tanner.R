0ec2f7f86585e09f5f9bdf72a82eb0e44659c2c5
Katie Palof <katie.palof@alaska.gov>
2018-11-16 09:53:31 -0900

    updated notes

--- code/TCcode/figures_1_and_2_Tanner.R
+++ code/TCcode/figures_1_and_2_Tanner.R
@@ -2,10 +2,9 @@
 # This script is a work in progress to develop figures like those currently used to view the 
 #     stock health of crab species in Southeast.  Current figures are in SigmaPlot. 
 #     Figure 1: regional biomass from CSA estimate - using ONLY survey areas 
-#                 - does NOT use current year's estimate.  Uses the estimate reported in the document each year
-#                 - to update this file, add most recent year from excel "Tanner Matrix cur_yr.xlsx" 
-#                   use pink cells labeled "put this in sigma plot graph"
-#             **FIX** yes Ben this is ridiculous, I'm working on creating these values from the actual data
+#                 - As of 2018 uses current years model output.  
+#                 - Needs to be adjusted in early years for areas that weren't surveyed at the beginning of the 
+#                     time series - see below.
 #     Figure 2: Harvest and standardized commercial CPUE (based on the year with the fewest pot lifts - 2008/09)  
 #               - pot lifts in that season were 12,521
 
